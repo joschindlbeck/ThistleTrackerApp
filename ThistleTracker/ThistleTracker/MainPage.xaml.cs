84170f3f843c5ab8a3cba79a9e8a98ec04dd3a08
josch <josch@DESKTOP-PRCAM22>
2020-06-03 20:43:45 +0200

    Projektdateien hinzufügen.

---
+++ ThistleTracker/ThistleTracker/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ThistleTracker
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        // Application class
+        ThistleTracker ThistleTracker = new ThistleTracker();
+        public ObservableCollection<WeedSpot> WeedSpots { get { return ThistleTracker._spots; } }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Item source for list view
+            SpotsListView.ItemsSource = ThistleTracker._spots;
+
+            // test add spot
+            /*
+            WeedSpot spot = new WeedSpot("Test", 1.1, 12.3);
+            ThistleTracker._spots.Add(spot);
+            WeedSpot spot2 = new WeedSpot("Test2", 1.1, 12.3);
+            ThistleTracker._spots.Add(spot2);
+            */
+        }
+
+
+        private async void WeedButton_Clicked(object sender, EventArgs e)
+        {
+            // Weed button clicked
+            // get geolocation
+            var location = await ThistleTracker.getLatLongAsync();
+
+            // add spot
+            WeedSpot spot = new WeedSpot(((Button)sender).Text, location.Latitude, location.Longitude);
+            ThistleTracker._spots.Add(spot);
+
+        }
+    }
+}
---
+++ ThistleTracker/ThistleTracker/ThistleTracker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace ThistleTracker
+{
+    public class ThistleTracker
+    {
+        // List of recorded spots
+        public ObservableCollection<WeedSpot> _spots = new ObservableCollection<WeedSpot>();
+
+        public async System.Threading.Tasks.Task<Location> getLatLongAsync()
+        {
+            try
+            {
+                // Geo Request
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                // Return                  
+                if (location != null)
+                {
+                    return location;
+                }
+                else
+                {
+                    return new Location(0, 0);
+                }
+
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                Debug.WriteLine(fnsEx.Message);
+                return new Location(0, 0);
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                Debug.WriteLine(fneEx.Message);
+                return new Location(0, 0);
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                Debug.WriteLine(pEx.Message);
+                return new Location(0, 0);
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                Debug.WriteLine(ex.Message);
+                return new Location(0, 0);
+            }
+        }
+    }
+
+}
+
+public class WeedSpot
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        
+        public string DisplayName
+    {
+        get
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append(": ");
+            sb.Append(Latitude);
+            sb.Append(", ");
+            sb.Append(Longitude);
+            return sb.ToString();
+        }
+    }
+
+        public WeedSpot(string name, double lat, double lon)
+        {
+            Name = name;
+            Latitude = lat;
+            Longitude = lon;
+        }
+}
